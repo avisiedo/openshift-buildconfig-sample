1c225f70775c05ead690afaf9dcd35f65bcdaa47
Alejandro Visiedo <avisiedo@redhat.com>
2022-04-07 15:14:56 +0200

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+MSG="Hello World 2"
+
 function _sighandler {
 	echo "Exiting by $1"
 	exit 0
@@ -8,5 +10,5 @@ function _sighandler {
 trap "_sighandler SIGTERM" SIGTERM
 trap "_sighandler SIGINT" SIGINT
 
-while true; do echo "Hello World"; sleep 2; done
+while true; do echo "$MSG"; sleep 2; done
 
